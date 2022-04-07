8dd721cc64c3151b521c89f35813589f05402202
1099238610 <68647376+1099238610@users.noreply.github.com>
2022-04-07 13:04:25 +0800

    Initial commit

---
+++ src/main/java/com/myfin/MyFinApplication.java
@@ -0,0 +1,16 @@
+package com.myfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zihang Gao
+ */
+@SpringBootApplication
+public class MyFinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFinApplication.class, args);
+    }
+
+}
