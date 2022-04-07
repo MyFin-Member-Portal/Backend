1c8898c1f2a04bf20e8d2e8dbb1bc8995eb60ead
1099238610 <68647376+1099238610@users.noreply.github.com>
2022-04-07 18:18:50 +0800

    implement a test login API

--- src/main/java/com/myfin/controller/LoginController.java
+++ src/main/java/com/myfin/controller/LoginController.java
@@ -1,4 +1,19 @@
 package com.myfin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zihang Gao
+ */
+
+@RestController
+@RequestMapping("/myfin")
 public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "Success";
+    }
+    
 }
