551dec63c137833865d0564ab3308e7a244f5dc0
1099238610 <68647376+1099238610@users.noreply.github.com>
2022-04-07 16:25:31 +0800

    Fix a mybatis bug

--- src/main/java/com/myfin/MyFinApplication.java
+++ src/main/java/com/myfin/MyFinApplication.java
@@ -2,11 +2,12 @@ package com.myfin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author Zihang Gao
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MyFinApplication {
 
     public static void main(String[] args) {
