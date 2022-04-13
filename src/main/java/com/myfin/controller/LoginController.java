a1f2bc8bd069ef0a0474c005b5ba18c7b67d3a22
1099238610 <68647376+1099238610@users.noreply.github.com>
2022-04-13 15:12:40 +0800

    add login service and controller

--- src/main/java/com/myfin/controller/LoginController.java
+++ src/main/java/com/myfin/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.myfin.controller;
 
+import com.myfin.base.Response;
+import com.myfin.base.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/myfin")
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
-        return "Success";
+    public Result<Object> login(){
+        return Response.success(null);
     }
     
 }
