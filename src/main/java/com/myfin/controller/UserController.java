077460a356c30c11fc043faec6e1a6084ba70266
YuzhuoMa816 <71178309+YuzhuoMa816@users.noreply.github.com>
2022-07-03 20:45:14 +0800

    change update from post to Put

--- src/main/java/com/myfin/controller/UserController.java
+++ src/main/java/com/myfin/controller/UserController.java
@@ -28,18 +28,18 @@ public class UserController {
         if (currentUser == null){
             return Response.fail("no such user");
         }
-        return Response.success("Name:"+currentUser.getUser_name()+
-                        " Email"+currentUser.getUser_email()+
-                        " PhoneNumber:"+currentUser.getUser_phone()+
-                        " Address:"+currentUser.getUser_address()+
-                        " Gender:"+currentUser.getUser_gender()+
-                        " Nationality:"+currentUser.getUser_nationality()+
-                        " Heritage:"+currentUser.getUser_heritage()+
-                        " Language"+currentUser.getUser_language());
+        return Response.success("Name: "+currentUser.getUser_name()+
+                        " Email: "+currentUser.getUser_email()+
+                        " PhoneNumber: "+currentUser.getUser_phone()+
+                        " Address: "+currentUser.getUser_address()+
+                        " Gender: "+currentUser.getUser_gender()+
+                        " Nationality: "+currentUser.getUser_nationality()+
+                        " Heritage: "+currentUser.getUser_heritage()+
+                        " Language: "+currentUser.getUser_language());
     }
 
 
-    @PostMapping("/updateUserBasicProfile")
+    @PutMapping("/updateUserBasicProfile")
     public Result<Object> updateUser(@RequestBody UserInfoUpdateRequest userInfoUpdateRequest){
 
         String currentPhoneNumber = userInfoUpdateRequest.getUserPhoneNumber();
@@ -52,7 +52,7 @@ public class UserController {
         String currentEmail = userInfoUpdateRequest.getUserEmail();
         int currentUserId = userInfoUpdateRequest.getUserId();
 
-        int updateResult = userService.updateUserProfile(currentPhoneNumber,
+        int resultUserId = userService.updateUserProfile(currentPhoneNumber,
                 currentAddress,
                 currentGender,
                 currentNationality,
@@ -63,8 +63,7 @@ public class UserController {
                 currentUserId);
 
 
-
-        return Response.success("Update success"+updateResult);
+        return Response.success(resultUserId);
     }
 }
 
